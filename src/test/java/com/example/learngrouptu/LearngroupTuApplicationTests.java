725ce4c4dd71263b5c4e499ed4763c8a6e75d978
Lasse <l_cezanne19@cs.uni-kl.de>
2021-05-07 16:14:59 +0200

    Push Springboot

---
+++ src/test/java/com/example/learngrouptu/LearngroupTuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learngrouptu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearngroupTuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
