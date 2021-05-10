e7d35ce8f46c7facef0f364c634faed456714043
vlad1 <v_mihalea17@cs.uni-kl.de>
2021-05-10 18:34:27 +0200

    annance zweite entwurf

--- src/main/java/com/example/learngrouptu/LearngroupTuApplication.java
+++ src/main/java/com/example/learngrouptu/LearngroupTuApplication.java
@@ -2,25 +2,26 @@ package com.example.learngrouptu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class LearngroupTuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearngroupTuApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myVorlesung", defaultValue =  "World", required = true) String name,
-                           @RequestParam(value = "choice", required = true) String choice,
-                           @RequestParam(value = "email", required = true) String email,
-                           @RequestParam(value = "Nachricht", required = false) String nachricht) {
-        return String.format("Hello %s! %s %s %s", name, choice,email,nachricht);
+
+
+    @RequestMapping("/annance_created")
+    public ModelAndView index () {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("homepage");
+        return modelAndView;
+
     }
 }
 
