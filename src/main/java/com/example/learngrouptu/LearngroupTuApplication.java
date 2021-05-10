9711a70231b100ab46e28737c7a5c5ff24c72710
vlad1 <v_mihalea17@cs.uni-kl.de>
2021-05-10 16:48:00 +0200

    Annance erst entwurf

--- src/main/java/com/example/learngrouptu/LearngroupTuApplication.java
+++ src/main/java/com/example/learngrouptu/LearngroupTuApplication.java
@@ -3,6 +3,7 @@ package com.example.learngrouptu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,13 @@ public class LearngroupTuApplication {
     }
 
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue =  "World") String name) {
-        return String.format("Hello %s!", name);
+    public String sayHello(@RequestParam(value = "myVorlesung", defaultValue =  "World", required = true) String name,
+                           @RequestParam(value = "choice", required = true) String choice,
+                           @RequestParam(value = "email", required = true) String email,
+                           @RequestParam(value = "Nachricht", required = false) String nachricht) {
+        return String.format("Hello %s! %s %s %s", name, choice,email,nachricht);
     }
-
 }
+
+
+
