5105716cec23468cb287159ca09e0dbd0a44fa70
andreas <a_nowack18@cs.uni-kl.de>
2021-05-12 14:57:52 +0200

    DB run

--- src/main/java/com/example/learngrouptu/LearngroupTuApplication.java
+++ src/main/java/com/example/learngrouptu/LearngroupTuApplication.java
@@ -6,12 +6,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.sql.*;
+//inport java.SQLException;
+
 @SpringBootApplication
 @RestController
 public class LearngroupTuApplication {
 
     public static void main(String[] args) {
+        String typString = null;
         SpringApplication.run(LearngroupTuApplication.class, args);
+
+        String jdbcUrl = "jdbc:sqlite:/C:\\sqlite\\db\\LearngroupTu_DB.db";
+        try{
+            Connection connection = DriverManager.getConnection(jdbcUrl);
+            String sql =" SELECT * FROM table_userinfo";
+
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            while(result.next()){
+                Integer id = result.getInt("ID");
+                String name = result.getString("Vorlesung");
+                String kontakt = result.getString("Kontakt");
+                Integer typ = result.getInt("Typ");
+
+                if(typ==1){
+                    typString = "Lerngruppe";
+                } else {
+                    typString = "Uebungsgruppe";}
+
+                System.out.println(id + "|" + name + " | " + kontakt + "|" + typString);
+            }
+
+        } catch (SQLException e){
+            System.out.println("Error connecting to SQLite database");
+        }
+
+
     }
 
     @GetMapping("/hello")
