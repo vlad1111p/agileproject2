26d96841fb8f879608f13cdcc33fb6e520779e49
-LXN- <f_homola19@cs.uni-kl.de>
2021-05-17 19:32:42 +0200

    Model und Repository erneut hinzugefügt

--- src/main/java/com/learngrouptu/controller/AnnoncenController.java
+++ src/main/java/com/learngrouptu/controller/AnnoncenController.java
@@ -1,27 +1,43 @@
 package com.learngrouptu.controller;
 
+import com.learngrouptu.model.AnnonceRepository;
+import com.learngrouptu.model.Annonce;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AnnoncenController {
+
+    private final AnnonceRepository annonceRepository;
+
+    @Autowired
+    public AnnoncenController(AnnonceRepository annonceRepository) {
+        this.annonceRepository = annonceRepository;
+    }
 
     @RequestMapping(value = "/annonceErstellen", method = RequestMethod.GET)
     public String showAnnonceErstellen(){return "annonceErstellen";}
 
     @RequestMapping(value = "/annonceEinsehen", method = RequestMethod.GET)
-    public String showAnnonceEinsehen(Model model){model.addAttribute("annoncen", annonceRepository.findAll());
+    public String showAnnonceEinsehen(Model model){
+        model.addAttribute("annoncen", annonceRepository.findAll());
         return "annonceEinsehen";}
 
+    /*
     @PostMapping("/annonce_created")
-    public String SetAnnonce (@RequestParam(value = "vorlName", required = true) String vorlName,
+    public String setAnnonce (@RequestParam(value = "vorlName", required = true) String vorlName,
                               @RequestParam(value = "choice", required = true) String choice,
                               @RequestParam(value = "kontakt", required = true) String kontakt,
                               @RequestParam(value = "Nachricht", required = true) String nachricht) {
+        annonceRepository.save(annonce);
+        return "annonceEinsehen";
+    }
+    */
+    @PostMapping("/annonce_created")
+    public String setAnnonce(@ModelAttribute("annonce") Annonce annonce, Model model){
         annonceRepository.save(annonce);
         return "annonceEinsehen";
     }
