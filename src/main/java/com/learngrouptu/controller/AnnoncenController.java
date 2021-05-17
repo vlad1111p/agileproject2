b82cf74e0ab72786d7d60230b1c76e2afb269587
j_schneide18 <j_schneide18@cs.uni-kl.de>
2021-05-17 18:34:03 +0200

    Annoncen Model erstellt, Annoncen repo erstellt, Annoncen einsehen erstellt

--- src/main/java/com/learngrouptu/controller/AnnoncenController.java
+++ src/main/java/com/learngrouptu/controller/AnnoncenController.java
@@ -1,8 +1,11 @@
 package com.learngrouptu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AnnoncenController {
@@ -11,5 +14,15 @@ public class AnnoncenController {
     public String showAnnonceErstellen(){return "annonceErstellen";}
 
     @RequestMapping(value = "/annonceEinsehen", method = RequestMethod.GET)
-    public String showAnnonceEinsehen(){return "annonceEinsehen";}
+    public String showAnnonceEinsehen(Model model){model.addAttribute("annoncen", annonceRepository.findAll());
+        return "annonceEinsehen";}
+
+    @PostMapping("/annonce_created")
+    public String SetAnnonce (@RequestParam(value = "vorlName", required = true) String vorlName,
+                              @RequestParam(value = "choice", required = true) String choice,
+                              @RequestParam(value = "kontakt", required = true) String kontakt,
+                              @RequestParam(value = "Nachricht", required = true) String nachricht) {
+        annonceRepository.save(annonce);
+        return "annonceEinsehen";
+    }
 }
