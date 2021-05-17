e51cdcfbb4a6b1f4e4423dc0407c532b3fc706fb
-LXN- <f_homola19@cs.uni-kl.de>
2021-05-17 16:03:11 +0200

    Seite für Home, Annonce Einsehen erstellt Controller für Annoncen erstellt Controller für Home Seite erstellt

--- src/main/java/com/learngrouptu/LearngroupTuApplication.java
+++ src/main/java/com/learngrouptu/LearngroupTuApplication.java
@@ -2,6 +2,9 @@ package com.learngrouptu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,8 +14,6 @@ public class LearngroupTuApplication {
     public static void main(String[] args) {
         SpringApplication.run(LearngroupTuApplication.class, args);
     }
-
-
 
 
 }
