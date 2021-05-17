e08f3a0464ceb8b07be66e6cc649366b0a3217d2
Lasse <l_cezanne19@cs.uni-kl.de>
2021-05-17 17:58:38 +0200

    Fix Tests for Linux (Lasse)

--- src/test/java/com/learngrouptu/homepage/AnnonceTest.java
+++ src/test/java/com/learngrouptu/homepage/AnnonceTest.java
@@ -1,6 +1,7 @@
 package com.learngrouptu.homepage;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,13 +12,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AnnonceTest {
 
-    @Test
-    public void testUrlCorrectForCorrectInput() throws InterruptedException {
-
+    public ChromeDriver init() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
+        if (System.getProperty("os.name").contains("Linux")) {
+            System.setProperty("webdriver.chrome.driver", "Linuxstuff/chromedriver");
+            options.addArguments("--remote-debugging-port=9222");
+        }
         ChromeDriver driver = new ChromeDriver(options);
         driver.get("http://localhost:8080/");
+        return driver;
+    }
+
+    @Test
+    public void testUrlCorrectForCorrectInput() throws InterruptedException {
+        ChromeDriver driver = init();
         driver.findElement(By.id("vorlName")).sendKeys("vlad");
         driver.findElement(By.id("kontakt")).sendKeys("vlad1111p@gmail.com");
         Select objSelect =new Select(driver.findElement(By.id("choice")));
@@ -27,17 +36,11 @@ class AnnonceTest {
         String expectedUrl="http://localhost:8080/annonce_created?vorlName=vlad&choice=Lerngruppe&kontakt=vlad1111p%40gmail.com&Nachricht=";
 
         assertEquals(expectedUrl, realUrl);
+        driver.close();
     }
     @Test
     public void testContentCorrectForCorrectInput() throws InterruptedException {
-
-
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.get("http://localhost:8080/");
-
+        ChromeDriver driver = init();
         driver.findElement(By.id("vorlName")).sendKeys("vlad");
         driver.findElement(By.id("kontakt")).sendKeys("vlad1111p@gmail.com");
         Select objSelect =new Select(driver.findElement(By.id("choice")));
@@ -47,16 +50,11 @@ class AnnonceTest {
         String realContent=driver.getPageSource().toString();
         String expectedContent="vlad Lerngruppe vlad1111p@gmail.com testnachricht";
         Assertions.assertTrue(realContent.contains(expectedContent));
+        driver.close();
     }
     @Test
     public void testUrlCorrectForWrongInput() throws InterruptedException {
-
-
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.get("http://localhost:8080/");
+        ChromeDriver driver = init();
         driver.findElement(By.id("kontakt")).sendKeys("vlad1111p@gmail.com");
         Select objSelect =new Select(driver.findElement(By.id("choice")));
         objSelect.selectByVisibleText("Lerngruppe");
@@ -64,19 +62,12 @@ class AnnonceTest {
         driver.findElement(By.id("button1")).click();
         String after=driver.getCurrentUrl();
         assertEquals(before, after);
+        driver.close();
     }
 
     @Test
     public void testUrlCorrectForWrongAndCorrectInput() throws InterruptedException {
-
-
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.get("http://localhost:8080/");
-
-
+        ChromeDriver driver = init();
         driver.findElement(By.id("kontakt")).sendKeys("vlad1111p@gmail.com");
 
         String before=driver.getCurrentUrl();
@@ -88,6 +79,7 @@ class AnnonceTest {
         driver.findElement(By.id("button1")).click();
         after=driver.getCurrentUrl();
         assertNotEquals(before,after);
+        driver.close();
     }
 
 
