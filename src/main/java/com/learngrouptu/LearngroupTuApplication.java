6b6f7d93a3e45a6c9a6b4959f59d8df210a265e5
andreas <a_nowack18@cs.uni-kl.de>
2021-05-25 17:37:34 +0200

    DB + Datum cleanup

--- src/main/java/com/learngrouptu/LearngroupTuApplication.java
+++ src/main/java/com/learngrouptu/LearngroupTuApplication.java
@@ -16,42 +16,7 @@ import java.sql.*;
 public class LearngroupTuApplication {
 
     public static void main(String[] args) {
-        String typString = null;
         SpringApplication.run(LearngroupTuApplication.class, args);
-
-
-        String jdbcUrl = "jdbc:sqlite:./LearngroupTU_DB.db";
-        try{
-
-
-            Connection connection = DriverManager.getConnection(jdbcUrl);
-
-
-            String sql ="SELECT * FROM annonce";
-
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(sql);
-
-            while(result.next()){
-                Integer id = result.getInt("aID");
-                String name = result.getString("Vorlesung");
-                String kontakt = result.getString("Kontakt");
-                String typ = result.getString("Typ");
-                String nachricht = result.getString("Nachricht");
-
-                if(typ=="Lerngruppe"){
-                    typString = "Lerngruppe";
-                } else {
-                    typString = "Uebungsgruppe";}
-
-                System.out.println(id + "|" + name + " | " + kontakt + "|" + typString + "|" + nachricht);
-            }
-
-        } catch (SQLException e){
-            System.out.println(e.getMessage());
-            System.out.println("Error connecting to SQLite database");
-        }
-
 
     }
 
