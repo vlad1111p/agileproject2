fc2a332991e7c306ce0aa672ea2cbe88dfe2c30d
andreas <a_nowack18@cs.uni-kl.de>
2021-05-25 18:17:55 +0200

    .

--- src/main/java/com/learngrouptu/LearngroupTuApplication.java
+++ src/main/java/com/learngrouptu/LearngroupTuApplication.java
@@ -2,9 +2,6 @@ package com.learngrouptu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,10 +14,7 @@ public class LearngroupTuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearngroupTuApplication.class, args);
-
     }
-
-
 }
 
 
