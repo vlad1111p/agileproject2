4e39932744f925d68c629d1465c1cf44e75b37ec
vlad1 <v_mihalea17@cs.uni-kl.de>
2021-05-26 17:16:28 +0200

    working version

--- src/main/java/com/learngrouptu/controller/VorlesungController.java
+++ src/main/java/com/learngrouptu/controller/VorlesungController.java
@@ -1,5 +1,7 @@
 package com.learngrouptu.controller;
 
+import com.learngrouptu.models.Annonce;
+import com.learngrouptu.models.AnnonceRepository;
 import com.learngrouptu.models.Vorlesung;
 import com.learngrouptu.models.VorlesungRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class VorlesungController {
 
     private final VorlesungRepository vorlesungRepository;
+    private final AnnonceRepository annonceRepository;
+
     @Autowired
-    public VorlesungController(VorlesungRepository vorlesungRepository){this.vorlesungRepository = vorlesungRepository;}
+    public VorlesungController(VorlesungRepository vorlesungRepository,AnnonceRepository annonceRepository){this.vorlesungRepository = vorlesungRepository;
+        this.annonceRepository = annonceRepository;}
+
+
 
 
     @GetMapping("/vorlesungsubersicht")
@@ -29,7 +39,21 @@ public class VorlesungController {
         return "vorlesungsubersichterstellen";
     }
 
+    @GetMapping("/vorlesungsgruppeerstellen")
+    public String showVorlesungGruppe(Model model, Annonce annonce,@RequestParam("id") Integer id){
+        model.addAttribute("vorlesungsubersicht1", vorlesungRepository.getOne(id).getTitel());
+        return "annonceErstellenNeu";
+    }
 
+    @GetMapping("/vorlesungsgruppesuchen")
+    public String showVorlesungGruppeErstellen(Model model,@RequestParam("id1") String id1){
+         List<Annonce> vorliterator = annonceRepository.findAll();
+         List<Annonce> vorllist = vorliterator.stream()
+               .filter(person -> person.getVorlName().matches(id1))
+                .collect(Collectors.toList());
+        model.addAttribute("annoncen", vorllist);
+        return "annonceEinsehen";
+    }
     @PostMapping(value="/vorlesungadd")
     public String addUser(@Valid Vorlesung vorlesung, BindingResult result, Model model) {
         if (result.hasErrors()) {
