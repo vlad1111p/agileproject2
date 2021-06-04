ce29970458f8e6f249cb694ea418fb2248656b78
-LXN- <f_homola19@cs.uni-kl.de>
2021-06-04 16:34:24 +0200

    Spring Security zugefügt

--- src/main/java/com/learngrouptu/LearngroupTuApplication.java
+++ src/main/java/com/learngrouptu/LearngroupTuApplication.java
@@ -2,14 +2,14 @@ package com.learngrouptu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import java.sql.*;
-//inport java.SQLException;
 
-@SpringBootApplication
 @RestController
+@SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
 public class LearngroupTuApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/learngrouptu/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.learngrouptu;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .withUser("testuser")
+                .password(encoder.encode("testpassword"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(encoder.encode("admin"))
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/anonymous*").anonymous()
+                .antMatchers("/login*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/home.html", true)
+                .failureUrl("/login.html?error=true")
+                //.failureHandler(authenticationFailureHandler())
+                .and()
+                .logout()
+                .logoutUrl("/perform_logout")
+                .deleteCookies("JSESSIONID");
+                //.logoutSuccessHandler(logoutSuccessHandler());
+}
