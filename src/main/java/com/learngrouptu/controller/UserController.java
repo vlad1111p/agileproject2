3170ea470b4eafa3a1da8208e1bdb497a0cd2be9
Lasse <l_cezanne19@cs.uni-kl.de>
2021-06-10 16:31:30 +0200

    Fixed messages for missing inputs, but not working duplicate detection etc

--- src/main/java/com/learngrouptu/controller/UserController.java
+++ src/main/java/com/learngrouptu/controller/UserController.java
@@ -4,17 +4,24 @@ import com.learngrouptu.DTO.RegisterDTO;
 import com.learngrouptu.models.Annonce;
 import com.learngrouptu.models.User;
 import com.learngrouptu.models.UserRepository;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class UserController {
@@ -27,13 +34,28 @@ public class UserController {
     @Autowired
     public UserController(UserRepository userRepository) {this.userRepository = userRepository;}
 
-    @PostMapping("/register")
-    public String showRegister(User user, Model model){return "register";}
+    @RequestMapping("/register")
+    public String showRegister(User user, Model model){
+        return "register";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterGet(User user, Model model){
+        System.out.println(model.asMap());
+        return "register";
+    }
+
+    public ModelAndView redirect(Model model) {
+        ModelAndView mav = new ModelAndView("redirect:register");
+        mav.addAllObjects(model.asMap());
+        return mav;
+    }
 
     @PostMapping("/perform_register")
     public String addUser(@Valid User user, BindingResult result, Model model) {
 
         if (result.hasErrors()) {
+            System.out.println("Something went wrong");
             return "redirect:register";
         }
 
@@ -44,9 +66,6 @@ public class UserController {
            else if (userRepository.findByEmail(user.getEmail()) != null) {
                throw new EmailDuplicateException();
            }
-           else if (user.getPassword() == null) {
-               throw new PasswordMissingException();
-           }
            else {
                user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
                //Test for turning off password encoding by commenting encryption function
@@ -56,18 +75,14 @@ public class UserController {
         }
         catch (UserDuplicateException e) {
                 model.addAttribute("usererror", new Object());
-                return showRegister(user, model);
+                return "register";
 
                 //return "redirect:/register?error=usererror";
 
         }
         catch (EmailDuplicateException e) {
                 model.addAttribute("emailerror", new Object());
-                return showRegister(user, model);
-        }
-        catch (PasswordMissingException e){
-                model.addAttribute("passworderror", new Object());
-                return showRegister(user, model);
+                return "redirect:register";
         }
     }
 
