7e2619fc4175f8e30667799d4374f314f0f742fa
andreas <a_nowack18@cs.uni-kl.de>
2021-06-21 16:29:54 +0200

    solved the problem of static resources being intercepted

--- src/main/java/com/learngrouptu/SecurityConfig.java
+++ src/main/java/com/learngrouptu/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,7 +20,8 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
 
 
     @Autowired
@@ -29,12 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder()
+    {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
@@ -56,24 +60,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 */
 
-/*
+    /*
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            PasswordEncoder encoder =
+                    PasswordEncoderFactories.createDelegatingPasswordEncoder();
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("testuser")
+                    .password(encoder.encode("testpassword"))
+                    .roles("USER")
+                    .and()
+                    .withUser("admin")
+                    .password(encoder.encode("admin"))
+                    .roles("USER", "ADMIN");
+        }
+    */
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder =
-                PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth
-                .inMemoryAuthentication()
-                .withUser("testuser")
-                .password(encoder.encode("testpassword"))
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password(encoder.encode("admin"))
-                .roles("USER", "ADMIN");
-    }
-*/
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    protected void configure(final HttpSecurity http) throws Exception
+    {
         http
                 .csrf().disable()
                 .authorizeRequests()
@@ -81,6 +86,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/anonymous*").anonymous()
                 .antMatchers("/login*").permitAll()
+                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -94,5 +101,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/perform_logout")
                 .deleteCookies("JSESSIONID");
         //.logoutSuccessHandler(logoutSuccessHandler());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/assets/**");
     }
 }
