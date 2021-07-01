0687773760f3d895e80428bc512f4445864beb86
Lasse <l_cezanne19@cs.uni-kl.de>
2021-07-01 15:11:10 +0200

    Added new html-page and Controller for visiting profiles

--- src/main/java/com/learngrouptu/controller/ProfileController.java
+++ src/main/java/com/learngrouptu/controller/ProfileController.java
@@ -2,14 +2,17 @@ package com.learngrouptu.controller;
 
 import com.learngrouptu.DTO.UserDTO;
 import com.learngrouptu.Exceptions.AbschlussNotAllowedException;
+import com.learngrouptu.Exceptions.UsernameDoesNotExistException;
 import com.learngrouptu.models.User;
 import com.learngrouptu.models.UserRepository;
 import com.learngrouptu.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProfileController {
@@ -25,6 +28,17 @@ public class ProfileController {
     public String showProfile(Model model, User user) {
         model.addAttribute("userProfile", userService.getCurrentUser());
         return "profilEinstellen";
+    }
+
+    @GetMapping("profilBesuchen")
+    public String visitProfile(Model model, @RequestParam(name="username" ,required = true) String username) throws UsernameDoesNotExistException {
+        if (userRepository.findUserByUsername(username) == null) {
+            throw new UsernameDoesNotExistException();
+        }
+        else {
+            model.addAttribute("userProfile", userRepository.findUserByUsername(username));
+            return "profilBesuchen";
+        }
     }
 
     @GetMapping("/profileChange")
