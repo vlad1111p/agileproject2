4eca004f411ff1784e40f487219bac26b861b1e6
vlad1 <v_mihalea17@cs.uni-kl.de>
2021-07-08 16:27:54 +0200

    finished version of vergessenesPasswortErsetzen

---
+++ src/main/java/com/learngrouptu/DTO/PasswordResetWithEmailDTO.java
@@ -0,0 +1,46 @@
+package com.learngrouptu.DTO;
+
+public class PasswordResetWithEmailDTO {
+
+    private String neuesPassword;
+    private String neuesPassword1;
+    private String email;
+
+
+    public PasswordResetWithEmailDTO() {
+        super();
+    }
+
+    public PasswordResetWithEmailDTO(String neuesPassword, String neuesPassword1, String email) {
+        super();
+
+        this.neuesPassword = neuesPassword;
+        this.neuesPassword1 = neuesPassword1;
+        this.email = email;
+    }
+
+
+    public String getNeuesPassword() {
+        return neuesPassword;
+    }
+
+    public void setNeuesPassword(String neuesPassword) {
+        this.neuesPassword = neuesPassword;
+    }
+
+    public String getNeuesPassword1() {
+        return neuesPassword1;
+    }
+
+    public void setNeuesPassword1(String neuesPassword1) {
+        this.neuesPassword1 = neuesPassword1;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
